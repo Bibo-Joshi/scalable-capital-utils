49f3eb54faf72a75e3e2e35feb49c258af11475b
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2024-01-30 17:07:28 +0100

    Fix black

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Example script that showcases how to use the `scutils` package."""
+
 from pprint import pprint
 
 from scutils import parse_html_transactions, sort_transactions_by_type, value_per_transaction_type
